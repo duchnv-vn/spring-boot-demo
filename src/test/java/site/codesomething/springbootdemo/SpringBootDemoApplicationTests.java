bf3a8a86809c5d275caac38dbdf4f30bce10d305
Duc Huynh <huynhnguyenvietduc@gmail.com>
2024-08-20 20:24:52 +0700

    feat(common): first commit

---
+++ src/test/java/site/codesomething/springbootdemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package site.codesomething.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
